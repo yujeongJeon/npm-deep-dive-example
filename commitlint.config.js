7d0265543d4ee66df3b86dad9bd9fb9ac9e94659
yujeongJeon <workingnewjeong@gmail.com>
2024-02-17 18:41:39 +0900

    🔧 commitlint 설정 + commit-msg에 추가

---
+++ commitlint.config.js
@@ -0,0 +1,47 @@
+const { gitmojis } = require('gitmojis')
+
+module.exports = {
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(:\w+:) (.+)/,
+      headerCorrespondence: ['type', 'subject'],
+    },
+  },
+  plugins: [
+    {
+      rules: {
+        'gitmoji-rule': ({ header }) => {
+          const commit = header.trim()
+          const [commitEmoji, ...rest] = commit.split(' ')
+          const commitMessage = rest.join(' ').trim()
+
+          if (!commitEmoji) {
+            return [false, 'Commit emoji is required.']
+          }
+
+          const gitmoji = gitmojis.find(
+            (_gitmoji) => _gitmoji.emoji === commitEmoji,
+          )
+
+          if (!gitmoji) {
+            return [
+              false,
+              `'${commitEmoji}' isn't valid Gitmoji. One of ${gitmojis.map(({ emoji }) => emoji).join(',')}`,
+            ]
+          }
+
+          if (!commitMessage) {
+            return [false, 'Commit message is required.']
+          }
+
+          return [true]
+        },
+      },
+    },
+  ],
+  rules: {
+    'gitmoji-rule': [2, 'always'],
+    'type-enum': [2, 'never'],
+    'subject-case': [2, 'never', ['sentence-case']],
+  },
+}
