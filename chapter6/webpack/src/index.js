954da26fb79d5c9fcc0cee3e87926db526fb3ba4
yceffort <yceffort@gmail.com>
2024-06-25 22:50:11 +0900

    🚀 webpack 예제 프로젝트 생성

---
+++ chapter6/webpack/src/index.js
@@ -0,0 +1,12 @@
+import './index.scss'
+import logoImage from '../public/images/logo.png'
+
+const imgElement = document.createElement('img')
+
+imgElement.src = logoImage
+imgElement.alt = 'Logo'
+imgElement.classList.add('logo')
+
+document.body.insertBefore(imgElement, document.body.firstChild)
+
+console.log('Webpack and SCSS Example with Dev Server')
---
+++ chapter6/webpack/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production'
+
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        minify: isProduction,
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css',
+      }),
+    ],
+    devServer: {
+      static: path.resolve(__dirname, 'dist'),
+      compress: true,
+      port: 9000,
+      open: true,
+    },
+    mode: isProduction ? 'production' : 'development',
+  }
+}
