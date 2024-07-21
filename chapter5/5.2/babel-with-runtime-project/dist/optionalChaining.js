b8283592e76f0a2b4a0d958681ad5ee045088046
yujeongJeon <workingnewjeong@gmail.com>
2024-07-21 20:06:49 +0900

    ✨ 5.2장: babel-with-runtime-project

---
+++ chapter5/5.2/babel-with-runtime-project/dist/optionalChaining.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var _Object$defineProperty = require("@babel/runtime-corejs3/core-js-stable/object/define-property");
+_Object$defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.pick = void 0;
+var pick = exports.pick = function pick(obj, keyChain) {
+  var keys = keyChain.split('.');
+  var result = obj;
+  for (var i = 0; i < keys.length; i++) {
+    var _obj$key;
+    var key = keys[i];
+    result = (_obj$key = obj[key]) !== null && _obj$key !== void 0 ? _obj$key : 'unknown';
+  }
+  return result;
+};
---
+++ chapter5/5.2/babel-with-runtime-project/src/optionalChaining.js
@@ -0,0 +1,12 @@
+export const pick = (obj, keyChain) => {
+  const keys = keyChain.split(".");
+
+  let result = obj;
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+
+    result = obj[key] ?? "unknown";
+  }
+
+  return result;
+};
