ef770daac176f0e9d7c590217b9b3522494bb1f1
yceffort <yceffort@gmail.com>
2024-08-02 00:51:08 +0900

    ✅ 0.2.1 사용

--- chapter9/repl/index.js
+++ chapter9/repl/index.js
@@ -1,3 +1,5 @@
-import { size } from "@yceffort/utils";
+import size from "@yceffort/utils/size";
+import isEmpty from "@yceffort/utils/isEmpty";
 
 console.log(size([1, 2, 3]));
+console.log(isEmpty([]));
