7570318e0c25b4185b80e28841fdb15f2c1674ad
yceffort <yceffort@gmail.com>
2024-08-03 17:33:47 +0900

    🚀 유틸 패키지 완성

--- chapter9/repl/index.js
+++ chapter9/repl/index.js
@@ -1,5 +1,15 @@
+import { size as rootSize } from "@yceffort/utils";
 import size from "@yceffort/utils/size";
 import isEmpty from "@yceffort/utils/isEmpty";
+import toLowerCase from "@yceffort/utils/toLowerCase";
+import toCamelCase from "@yceffort/utils/toCamelCase";
+
+console.log(rootSize === size);
 
 console.log(size([1, 2, 3]));
+
 console.log(isEmpty([]));
+
+console.log(toLowerCase("HELLO WORLD"));
+
+console.log(toCamelCase("hello world"));
--- chapter9/utils/toCamelCase.ts
+++ chapter9/utils/toCamelCase.ts
@@ -1,4 +1,4 @@
-import * as mod from "@luca/cases/";
+import * as mod from "@luca/cases";
 
 export default function toCamelCase(text: string): string {
   return mod.camelCase(text);
