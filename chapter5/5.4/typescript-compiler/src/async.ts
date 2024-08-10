6d5427853e13ed0f409f45dfa474bc5b170bd561
yujeongJeon <workingnewjeong@gmail.com>
2024-08-11 02:05:31 +0900

    typescript-compiler에 core-js 추가

--- chapter5/5.4/typescript-compiler/src/async.ts
+++ chapter5/5.4/typescript-compiler/src/async.ts
@@ -1,3 +1,6 @@
+import "core-js/modules/es.promise.js";
+import "core-js/modules/es.object.to-string.js"; // 구형 브라우저에서 Object.prototype.toString() 동작이 상이할 위험을 방지
+
 const asyncFunction = async () => {
   const result = await new Promise<number>((resolve) => {
     resolve(1000);
